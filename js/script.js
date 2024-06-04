1568012360abb8dd983c112561cc582327b81cec
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-06-04 11:15:16 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-(function createBoard() {
-  // Gameboard for tic-tac-toe
-  const gameBoard = [[], [], [], [], [], [], [], [], []];
-
-  return { gameBoard };
-})()
+// TODO: Create tic-tac-toe within the console
+// We need two players who have each Marker X and Marker O
+// We need to create the board and update each time players select where they
+// place their marker
 
 function createPlayer(name, marker) {
-  const getName = function () {
-    return console.log(`${name}`);
+  const getName = function() {
+    return name;
   };
 
-  const getMarker = function () {
-    return console.log(`${name} has marker: ${marker}`);
+  const getMarker = function() {
+    return marker;
   };
   return { getName, getMarker };
 }
+
+const gameBoard = (function() {
+  const board = [[], [], [], [], [], [], [], [], []];
+
+  const playerOne = createPlayer("PlayerOne", "X");
+  const playerTwo = createPlayer("PlayerTwo", "O");
+
+  return function generateBoard() {
+    const copyOfBoard = board.slice();
+    return { copyOfBoard, playerOne, playerTwo };
+  };
+})();
